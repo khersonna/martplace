d239290710d9dda110c65c3bcc8c721548e2b2d5
iryna.khersonna <iryna.khersonna@Irynas-MacBook-Pro.local>
2020-04-11 21:53:18 +0300

    added section product-slider changed products

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,20 @@
 $(function(){
 
 
-var miver = mixitup('.products__inner-box');
+    var miver = mixitup('.products__inner-holder');
 
-    
+      $(".rate-star").rateYo({
+          rating: 4.5,
+          starWidth: "12px",
+          readOnly: true
+      });
+
+      $('.product-slider__inner').slick({
+          dots: false,
+          arrows: true,
+          slidesToShow: 3,
+          slidesToScroll: 2,
+         prevArrow: '<button class="slick-arrow slick-prev"><img src="../images/icons/arrow-left.svg" alt=""></button>',
+         nextArrow: '<button class="slick-arrow slick-next"><img src="../images/icons/arrow-right.svg" alt=""></button>',
+      });
 });
