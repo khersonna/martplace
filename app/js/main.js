2866dbfcd406269ed27a139cd2fb479140193f4c
iryna.khersonna <iryna.khersonna@Irynas-MacBook-Pro.local>
2020-05-02 23:58:25 +0300

    added first part products, added font

--- app/js/main.js
+++ app/js/main.js
@@ -1,20 +1,32 @@
-$(function(){
+$(function () {
+
+
+    $(".rate-star").rateYo({
+        rating: 4.5,
+        starWidth: "12px",
+        readOnly: true
+    });
+
+    $('.product-slider__inner').slick({
+        dots: false,
+        arrows: true,
+        slidesToShow: 3,
+        slidesToScroll: 2,
+        prevArrow: '<button class="slick-arrow slick-prev"><img src="../images/icons/arrow-left.svg" alt=""></button>',
+        nextArrow: '<button class="slick-arrow slick-next"><img src="../images/icons/arrow-right.svg" alt=""></button>',
+    });
+
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 400,
+        from: 30,
+        to: 300,
+        grid: false,
+        prefix: "$"
+    });
+
 
 
     var miver = mixitup('.products__inner-holder');
-
-      $(".rate-star").rateYo({
-          rating: 4.5,
-          starWidth: "12px",
-          readOnly: true
-      });
-
-      $('.product-slider__inner').slick({
-          dots: false,
-          arrows: true,
-          slidesToShow: 3,
-          slidesToScroll: 2,
-         prevArrow: '<button class="slick-arrow slick-prev"><img src="../images/icons/arrow-left.svg" alt=""></button>',
-         nextArrow: '<button class="slick-arrow slick-next"><img src="../images/icons/arrow-right.svg" alt=""></button>',
-      });
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,39 @@
 let gulp = require('gulp'),
     sass = require('gulp-sass'),
     rename = require('gulp-rename'),
-    browserSync =require('browser-sync'),
+    browserSync = require('browser-sync'),
     autoprefixer = require('gulp-autoprefixer'),
     concat = require('gulp-concat'),
-    uglify= require('gulp-uglify');
-    cssmin = require('gulp-cssmin')
+    uglify = require('gulp-uglify');
+cssmin = require('gulp-cssmin')
 
 
-gulp.task('sass', function(){
-   return gulp.src('app/scss/**/*.scss')
-            .pipe(sass({outputStyle: 'expand'}))
-            .pipe(rename({suffix: '.min'}))
-            .pipe(autoprefixer({
-                overrideBrowserslist:['last 8 versions']
-            }))
-            .pipe(gulp.dest('app/css'))
-            .pipe(browserSync.reload({stream: true}))
-}); 
+gulp.task('sass', function () {
+    return gulp.src('app/scss/**/*.scss')
+        .pipe(sass({
+            outputStyle: 'expand'
+        }))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 8 versions']
+        }))
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
 
-gulp.task('style', function(){
+gulp.task('style', function () {
     return gulp.src([
-        'node_modules/normalize.css/normalize.css',
-        'node_modules/slick-carousel/slick/slick.css',
-        'node_modules/magnific-popup/dist/magnific-popup.css',
-        'node_modules/rateyo/src/jquery.rateyo.css'
-    ])
+            'node_modules/normalize.css/normalize.css',
+            'node_modules/slick-carousel/slick/slick.css',
+            'node_modules/magnific-popup/dist/magnific-popup.css',
+            'node_modules/rateyo/src/jquery.rateyo.css',
+            'node_modules/ion-rangeslider/css/ion.rangeSlider.css'
+
+        ])
 
         .pipe(concat('libs.min.css'))
         .pipe(cssmin())
@@ -33,13 +41,14 @@ gulp.task('style', function(){
 });
 
 
-gulp.task('script', function(){
+gulp.task('script', function () {
     return gulp.src([
-        'node_modules/slick-carousel/slick/slick.js',
-        'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
-        'node_modules/mixitup/dist/mixitup.js',
-        'node_modules/rateyo/src/jquery.rateyo.js'
-    ])
+            'node_modules/slick-carousel/slick/slick.js',
+            'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
+            'node_modules/mixitup/dist/mixitup.js',
+            'node_modules/rateyo/src/jquery.rateyo.js',
+            'node_modules/ion-rangeslider/js/ion.rangeSlider.js'
+        ])
 
         .pipe(concat('libs.min.js'))
         .pipe(uglify())
@@ -47,18 +56,22 @@ gulp.task('script', function(){
 });
 
 
-gulp.task('html', function(){
+gulp.task('html', function () {
     return gulp.src('app/*.html')
-    .pipe(browserSync.reload({stream: true}))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
 });
 
-gulp.task('js', function(){
+gulp.task('js', function () {
     return gulp.src('app/js/*.js')
-    .pipe(browserSync.reload({stream: true}))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
 });
 
 
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', function () {
     browserSync.init({
         server: {
             baseDir: "app/"
@@ -66,7 +79,7 @@ gulp.task('browser-sync', function() {
     });
 });
 
-gulp.task('watch', function(){
+gulp.task('watch', function () {
     gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'))
     gulp.watch('app/*.html', gulp.parallel('html'))
     gulp.watch('app/js/*.js', gulp.parallel('js'))
