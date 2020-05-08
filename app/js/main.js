02b455279e018c04abd1f30369fa1754fa0bb572
iryna.khersonna <iryna.khersonna@Irynas-MacBook-Pro.local>
2020-05-08 17:48:56 +0300

    changed structure of social icons,added page single-product, changed fonts icomoon

--- app/js/main.js
+++ app/js/main.js
@@ -38,6 +38,16 @@ $(function () {
 
     });
 
+    $('.single-product__tabs .tab').on('click', function (event) {
+        var id = $(this).attr('data-id');
+        $('.single-product__tabs').find('.tab-item').removeClass('active-tab').hide();
+        $('.single-product__tabs .tabs').find('.tab').removeClass('active');
+        $(this).addClass('active');
+        $('#' + id).addClass('active-tab').fadeIn();
+        return false;
+    });
+
+
 
     var miver = mixitup('.products__inner-holder');
 });
